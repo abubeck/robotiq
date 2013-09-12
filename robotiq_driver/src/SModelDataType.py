43d85dbf4fbd945c9ab9ab89c01ba3ad9dc11e68
Alexander Bubeck <alexander.bubeck@ipa.fraunhofer.de>
2013-09-12 17:50:31 +0200

    new refurbished robotiq driver

---
+++ robotiq_driver/src/SModelDataType.py
@@ -0,0 +1,47 @@
+class inputMsg:
+  def __init__(self):
+    self.gACT = 0
+    self.gMOD = 0
+    self.gGTO = 0
+    self.gIMC = 0
+    self.gSTA = 0
+    self.gDTA = 0
+    self.gDTB = 0
+    self.gDTC = 0
+    self.gDTS = 0
+    self.gFLT = 0
+    self.gPRA = 0
+    self.gPOA = 0
+    self.gCUA = 0
+    self.gPRB = 0
+    self.gPOB = 0
+    self.gCUB = 0
+    self.gPRC = 0
+    self.gPOC = 0
+    self.gCUC = 0
+    self.gPRS = 0
+    self.gPOS = 0
+    self.gCUS = 0
+
+
+class outputMsg:
+  def __init__(self):
+    self.rACT = 0
+    self.rMOD = 0
+    self.rGTO = 0
+    self.rATR = 0
+    self.rGLV = 0
+    self.rICF = 0
+    self.rICS = 0
+    self.rPRA = 0
+    self.rSPA = 0
+    self.rFRA = 0
+    self.rPRB = 0
+    self.rSPB = 0
+    self.rFRB = 0
+    self.rPRC = 0
+    self.rSPC = 0
+    self.rFRC = 0
+    self.rPRS = 0
+    self.rSPS = 0
+    self.rFRS = 0
--- robotiq_driver/src/baseSModel.py
+++ robotiq_driver/src/baseSModel.py
@@ -39,114 +39,110 @@ Module baseSModel: defines a base class for handling command and status of the R
 After being instanciated, a 'client' member must be added to the object. This client depends on the communication protocol used by the Gripper. As an example, the ROS node 'SModelTcpNode.py' instanciate a robotiqBaseSModel and adds a client defined in the module comModbusTcp.
 """
 
-from   robotiq_s_model_control.msg import _SModel_robot_input  as inputMsg
-from   robotiq_s_model_control.msg import _SModel_robot_output as outputMsg
+from SModelDataType import *
 
 class robotiqBaseSModel:
     """Base class (communication protocol agnostic) for sending commands and receiving the status of the Robotic S-Model gripper."""
     
     def __init__(self):
+      #Initiate output message as an empty list
+      self.message = []
 
-        #Initiate output message as an empty list
-        self.message = []
+      #Note: after the instantiation, a ".client" member must be added to the object
 
-        #Note: after the instantiation, a ".client" member must be added to the object
-        
         
     def verifyCommand(self, command):
-        """Function to verify that the value of each variable satisfy its limits."""
-   	   	
-   	#Verify that each variable is in its correct range
-   	command.rACT = max(0, command.rACT)
-   	command.rACT = min(1, command.rACT)
-   	
-   	command.rMOD = max(0, command.rMOD)
-   	command.rMOD = min(3, command.rMOD)
-   	
-   	command.rGTO = max(0, command.rGTO)
-   	command.rGTO = min(1, command.rGTO)
+      """Function to verify that the value of each variable satisfy its limits."""    
+      #Verify that each variable is in its correct range
+      command.rACT = max(0, command.rACT)
+      command.rACT = min(1, command.rACT)
 
-   	command.rATR = max(0, command.rATR)
-   	command.rATR = min(1, command.rATR)
+      command.rMOD = max(0, command.rMOD)
+      command.rMOD = min(3, command.rMOD)
 
-   	command.rGLV = max(0, command.rGLV)
-   	command.rGLV = min(1, command.rGLV)
+      command.rGTO = max(0, command.rGTO)
+      command.rGTO = min(1, command.rGTO)
 
-   	command.rICF = max(0, command.rICF)
-   	command.rICF = min(1, command.rICF)
+      command.rATR = max(0, command.rATR)
+      command.rATR = min(1, command.rATR)
 
-   	command.rICS = max(0, command.rICS)
-   	command.rICS = min(1, command.rICS)
-   	
-   	command.rPRA = max(0,   command.rPRA)
-   	command.rPRA = min(255, command.rPRA)   	
+      command.rGLV = max(0, command.rGLV)
+      command.rGLV = min(1, command.rGLV)
 
-   	command.rSPA = max(0,   command.rSPA)
-   	command.rSPA = min(255, command.rSPA)   	
+      command.rICF = max(0, command.rICF)
+      command.rICF = min(1, command.rICF)
 
-   	command.rFRA = max(0,   command.rFRA)
-   	command.rFRA = min(255, command.rFRA)   	
+      command.rICS = max(0, command.rICS)
+      command.rICS = min(1, command.rICS)
 
-   	command.rPRB = max(0,   command.rPRB)
-   	command.rPRB = min(255, command.rPRB)   	
+      command.rPRA = max(0,   command.rPRA)
+      command.rPRA = min(255, command.rPRA)     
 
-   	command.rSPB = max(0,   command.rSPB)
-   	command.rSPB = min(255, command.rSPB)   	
+      command.rSPA = max(0,   command.rSPA)
+      command.rSPA = min(255, command.rSPA)     
 
-   	command.rFRB = max(0,   command.rFRB)
-   	command.rFRB = min(255, command.rFRB)  
+      command.rFRA = max(0,   command.rFRA)
+      command.rFRA = min(255, command.rFRA)     
 
-   	command.rPRC = max(0,   command.rPRC)
-   	command.rPRC = min(255, command.rPRC)   	
+      command.rPRB = max(0,   command.rPRB)
+      command.rPRB = min(255, command.rPRB)     
 
-   	command.rSPC = max(0,   command.rSPC)
-   	command.rSPC = min(255, command.rSPC)   	
+      command.rSPB = max(0,   command.rSPB)
+      command.rSPB = min(255, command.rSPB)     
 
-   	command.rFRC = max(0,   command.rFRC)
-   	command.rFRC = min(255, command.rFRC)   
-   	
-   	command.rPRS = max(0,   command.rPRS)
-   	command.rPRS = min(255, command.rPRS)   	
+      command.rFRB = max(0,   command.rFRB)
+      command.rFRB = min(255, command.rFRB)  
 
-   	command.rSPS = max(0,   command.rSPS)
-   	command.rSPS = min(255, command.rSPS)   	
+      command.rPRC = max(0,   command.rPRC)
+      command.rPRC = min(255, command.rPRC)     
 
-   	command.rFRS = max(0,   command.rFRS)
-   	command.rFRS = min(255, command.rFRS) 
-   	
-   	#Return the modified command
-   	return command
-   	
+      command.rSPC = max(0,   command.rSPC)
+      command.rSPC = min(255, command.rSPC)     
+
+      command.rFRC = max(0,   command.rFRC)
+      command.rFRC = min(255, command.rFRC)   
+
+      command.rPRS = max(0,   command.rPRS)
+      command.rPRS = min(255, command.rPRS)     
+
+      command.rSPS = max(0,   command.rSPS)
+      command.rSPS = min(255, command.rSPS)     
+
+      command.rFRS = max(0,   command.rFRS)
+      command.rFRS = min(255, command.rFRS) 
+
+      #Return the modified command
+      return command
+    
     def refreshCommand(self, command):
-        """Function to update the command which will be sent during the next sendCommand() call."""
-        
-    	#Limit the value of each variable
-    	command = self.verifyCommand(command)
+      """Function to update the command which will be sent during the next sendCommand() call."""
+      #Limit the value of each variable
+      command = self.verifyCommand(command)
 
-        #Initiate command as an empty list
-        self.message = []
+      #Initiate command as an empty list
+      self.message = []
 
-        #Build the command with each output variable
-        self.message.append(command.rACT + (command.rMOD << 1) + (command.rGTO << 3) + (command.rATR << 4))
-        self.message.append(command.rGLV + (command.rICF << 2) + (command.rICS << 3))
-        self.message.append(0)
-        self.message.append(command.rPRA)
-        self.message.append(command.rSPA)
-        self.message.append(command.rFRA)
-        self.message.append(command.rPRB)
-        self.message.append(command.rSPB)
-        self.message.append(command.rFRB)
-        self.message.append(command.rPRC)
-        self.message.append(command.rSPC)
-        self.message.append(command.rFRC)
-        self.message.append(command.rPRS)
-        self.message.append(command.rSPS)
-        self.message.append(command.rFRS)        
+      #Build the command with each output variable
+      self.message.append(command.rACT + (command.rMOD << 1) + (command.rGTO << 3) + (command.rATR << 4))
+      self.message.append(command.rGLV + (command.rICF << 2) + (command.rICS << 3))
+      self.message.append(0)
+      self.message.append(command.rPRA)
+      self.message.append(command.rSPA)
+      self.message.append(command.rFRA)
+      self.message.append(command.rPRB)
+      self.message.append(command.rSPB)
+      self.message.append(command.rFRB)
+      self.message.append(command.rPRC)
+      self.message.append(command.rSPC)
+      self.message.append(command.rFRC)
+      self.message.append(command.rPRS)
+      self.message.append(command.rSPS)
+      self.message.append(command.rFRS)        
 
     def sendCommand(self):
-        """Send the command to the Gripper."""    
-        
-        self.client.sendCommand(self.message)
+      """Send the command to the Gripper."""    
+
+      self.client.sendCommand(self.message)
 
     def getStatus(self):
         """Request the status from the gripper and return it in the SModel_robot_input msg type."""    
@@ -155,7 +151,7 @@ class robotiqBaseSModel:
         status = self.client.getStatus(16);
 
         #Message to output
-        message = inputMsg.SModel_robot_input()
+        message = inputMsg()
 
         #Assign the values to their respective variables
         message.gACT = (status[0] >> 0) & 0x01;
